92115276ac5bdb60fa53c7b21eb921d88f4a1625
GerardoMac <centrodph@gmail.com>
2018-01-28 20:12:01 -0300

    build update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,12 @@ class Footer extends Component {
   render() {
     return (
       <div className="footerComponent">
-        <Link to="/">Home</Link> |
-        <Link to="/items?search=televisor&size=3">SearchResult</Link> |
-        <Link to="/items/992">ProductDetail</Link>
+        <Link to={process.env.PUBLIC_URL + '/'}>Home</Link> |
+        <Link to={process.env.PUBLIC_URL + '/items?search=televisor&size=3'}>
+          SearchResult
+        </Link>{' '}
+        |
+        <Link to={process.env.PUBLIC_URL + '/items/992'}>ProductDetail</Link>
       </div>
     );
   }
