f87005dc4e770b560bb8214c9b59fad26d11e4ee
GerardoMac <centrodph@gmail.com>
2018-01-29 06:20:38 -0300

    fix url

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <div className="footerComponent">
+        TEST
         <Link to="./">Home</Link> |
         <Link to="./items?search=televisor&size=3">SearchResult</Link> |
         <Link to="./items/992">ProductDetail</Link>
