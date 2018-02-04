b6d28b3f18cd79bfa7be4a617b540c36a3402d13
GerardoMac <centrodph@gmail.com>
2018-02-04 08:18:59 -0300

    default routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const static = require('../controllers/api');
 
 module.exports = app => {
+  /**
+   * static
+   */
   app.get('/', static.apiCtrl);
+  app.get('/items', static.apiCtrl);
+  app.get('/items/:id', static.apiCtrl);
+
+  /**
+   * API
+   */
   app.get('/search/:term', static.apiSearchCtrl);
   app.get('/api/items', static.apiSearchCtrl);
   app.get('/api/items/:id', static.apiProductCtrl);
